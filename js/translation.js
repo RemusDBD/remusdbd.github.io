d23b83531e8ed22e2032b67de92ad6f2303bb0db
Remus <127229497+RemusDBD@users.noreply.github.com>
2024-10-25 15:12:30 +0800

    add subtitle 4 for Astrologer

--- js/translation.js
+++ js/translation.js
@@ -22,6 +22,7 @@ function changeLang(lang) {
     $('#subtitle1').text('內容創作者 (知名於黎明死線DBD教學)');
     $('#subtitle2').text('獨立安全研究員 / 道德黑客 / CTF奪旗玩家');
     $('#subtitle3').text('投機者');
+    $('#subtitle4').text('占星師');
     $('#aboutme-link').text('關於我');
     $('#writeup-link').text('CTF解題報告');
     $('#tools-link').text('工具');
@@ -41,6 +42,7 @@ function changeLang(lang) {
     $('#subtitle1').text('Content Creator');
     $('#subtitle2').text('Independent Security Researcher / Ethical Hacker / CTF Player');
     $('#subtitle3').text('Speculator');
+    $('#subtitle4').text('Astrologer');
     $('#aboutme-link').text(' About Me');
     $('#writeup-link').text(' Writeup');
     $('#tools-link').text('Tools');
