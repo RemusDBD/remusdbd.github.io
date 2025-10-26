786563b156142a209a2b6fe150279318ca30611b
Remus <127229497+RemusDBD@users.noreply.github.com>
2025-10-27 00:53:17 +0800

    Update translation.js

--- js/translation.js
+++ js/translation.js
@@ -18,18 +18,10 @@ function changeLang(lang) {
   // Dynamic content update based on language selection
   if (lang === 'cn') {
     console.log('Switching to Chinese'); // Log specific action
-    $('#title').text('Remus 雷姆');
+    $('#title').text('「尋根，讓財富像流水一樣」');
     $('#subtitle1').text('你關心人類嗎?');
     $('#subtitle2').text('你要改革金融?');
-    $('#subtitle3').text('加入我');
-    $('#subtitle4').text('「尋根，讓財富像流水一樣」');
-    //$('#aboutme-link').text('關於我');
-    //$('#writeup-link').text('CTF解題報告');
-    //$('#tools-link').text('工具');
-    //$('#invest-link').text('投資專欄');
-    //$('#services-link').text('服務');
-    //$('#joinus-link').text('加入我們');
-    $('#contactus-link').text('聯絡我們');
+    $('#contactus-link').text('聯絡我');
     
     // Hide English content and show Chinese content
     $('.en').each(function () {
@@ -39,17 +31,9 @@ function changeLang(lang) {
     });
   } else if (lang === 'us') {
     console.log('Switching to English'); // Log specific action
-    $('#title').text('Remus');
+    $('#title').text('"Turning source into wealth — so it flows"');
     $('#subtitle1').text('You do care about people?');
     $('#subtitle2').text('You do want to revolutionise finance?');
-    $('#subtitle3').text('Join me');
-    $('#subtitle4').text('"Turning source into wealth — so it flows"');
-    //$('#aboutme-link').text(' About Me');
-    //$('#writeup-link').text(' Writeup');
-    //$('#tools-link').text('Tools');
-    //$('#invest-link').text(' Invest');
-    //$('#services-link').text(' Services');
-    //$('#joinus-link').text(' Join Us');
     $('#contactus-link').text(' Contact Us');
     
     // Hide Chinese content and show English content
